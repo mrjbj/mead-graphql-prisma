a7c627a1733ed00ce9406aa7ea1040d8a5741e2f
Jason Jones <jason@brucejones.biz>
2020-03-07 22:59:52 -0500

    Lesson 52: Updated config for ESLint with Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,26 @@
-// ---------------------------------------------------------------------------------------------------------
-// Configure GraphQL server object
-// ---------------------------------------------------------------------------------------------------------
-// typedefs: specifying GraphQL schema definition as filename allows import from external file          (1)
-// resolvers:
-//   - Query, Mutation & Subscription are reserved names for standard properties, defined by GraphQL
-//       - resolvers.Query{} contains a list of methods that query data from database.                  (2)
-//       - resolvers.Mutation{} constains a list of methods that change data in database.               (3)
-//       - resolvers.Subscription{} is like Query{} but pushes data when it changes rather than         (4)
-//         pulling data only upon request.
-//       - resolvers.<method> implements the queries or mutations declared in Query{} or Mutation{}
-//          - each resolvers.<method> function can return (or resolve) scalar types directly.
-//          - if resolvers.<method> needs to return a custom GraphQL type, (for example 'User'), then   (5)
-//            it will expect another resolver method with that same name to have been defined
-//            (e.g. resolvers.Users) and GraphQL runtime will call it iteratively for each row returned
-//            by the parent.  (The 'parent' argument will be populated with info concerning this).
-//          - resolvers.<method> is a function defined inside an object, so 'this' binding is important
-//            (e.g. use ES6 method syntax that preserves 'this' binding - do not use arrow functions).
-// context: setting context to db object makes it available to every resolver method in the entire
-//          system via ctx parameter (e.g. resolvers.me(parent, args, ctx, info))                       (6)
-// ---------------------------------------------------------------------------------------------------------
+/*
+ * ---------------------------------------------------------------------------------------------------------
+ * Configure GraphQL server object
+ * ---------------------------------------------------------------------------------------------------------
+ * typedefs: specifying GraphQL schema definition as filename allows import from external file          (1)
+ * resolvers:
+ *   - Query, Mutation & Subscription are reserved names for standard properties, defined by GraphQL
+ *       - resolvers.Query{} contains a list of methods that query data from database.                  (2)
+ *       - resolvers.Mutation{} constains a list of methods that change data in database.               (3)
+ *       - resolvers.Subscription{} is like Query{} but pushes data when it changes rather than         (4)
+ *         pulling data only upon request.
+ *       - resolvers.<method> implements the queries or mutations declared in Query{} or Mutation{}
+ *          - each resolvers.<method> function can return (or resolve) scalar types directly.
+ *          - if resolvers.<method> needs to return a custom GraphQL type, (for example 'User'), then   (5)
+ *            it will expect another resolver method with that same name to have been defined
+ *            (e.g. resolvers.Users) and GraphQL runtime will call it iteratively for each row returned
+ *            by the parent.  (The 'parent' argument will be populated with info concerning this).
+ *          - resolvers.<method> is a function defined inside an object, so 'this' binding is important
+ *            (e.g. use ES6 method syntax that preserves 'this' binding - do not use arrow functions).
+ * context: setting context to db object makes it available to every resolver method in the entire
+ *          system via ctx parameter (e.g. resolvers.me(parent, args, ctx, info))                       (6)
+ * ---------------------------------------------------------------------------------------------------------
+ */
 import { GraphQLServer, PubSub } from 'graphql-yoga'
 import { db } from './db'
 import { watchSchemaFiles } from './util/watchSchemaFiles'
@@ -55,5 +57,5 @@ const server = new GraphQLServer({
 })
 
 server.start(() => {
-  console.log("GraphQL server ready...")
+  console.log('GraphQL server ready...')
 })
--- src/prisma.ts
+++ src/prisma.ts
@@ -1,17 +1,58 @@
-import { Prisma } from "prisma-binding"
+import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
   typeDefs: './src/generated/prisma.graphql',
   endpoint: 'http://localhost:4466'
 })
 
-// prisma.query.users(undefined, '{id name  posts {id title comments{text } } }').then((data: any) => {
-//   console.log(JSON.stringify(data, null, 4))
-// })
+/*
+ * prisma.query.users(undefined, '{id name  posts {id title comments{text } } }').then((data: any) => {
+ *   console.log(JSON.stringify(data, null, 4))
+ * })
+ */
 
+/*
+ * const wrapper = async () => {
+ *   const data = await prisma.query.comments(undefined, "{id text author {id name email}}")
+ *   console.log(JSON.stringify(data, null, 2))
+ * }
+ */
+
+/*
+ * const wrapper = async () => {
+ *   const data = await prisma.mutation.createPost({
+ *     data: {
+ *       title: "Why I came to live amongst you on earth.",
+ *       body: "It's understandable you doubt and were lost.  I came to make it easy for you, to forgive and show you the way, so that you might see me and know more confidently what I think and how much I care.",
+ *       published: true,
+ *       author: { connect: { id: "ck7hxpakq04jg0824rluur2mj" } }
+ *     }
+ *   }, "{ id title body published}")
+ *   console.log(JSON.stringify(data, null, 2))
+ * }
+ */
+
+/*
+ * const wrapper = async () => {
+ *   await prisma.mutation.updatePost({
+ *     data: { published: true },
+ *     where: { id: "ck7idj39n074o0824o1d3k3b1" }
+ *   }, "{id title body published}"
+ *   )
+ *   const posts = await prisma.query.posts(undefined, "{id title body published}")
+ *   console.log(JSON.stringify(posts, null, 2))
+ * }
+ */
+const TAB_SPACES = 2
 const wrapper = async () => {
-  const data = await prisma.query.comments(undefined, "{id text author {id name email}}")
-  console.log(JSON.stringify(data, null, 2))
+  const jason = await prisma.mutation.createComment({
+    data: {
+      text: `I'll say it again... "Now that's a God worthy of worship!"`,
+      post: { connect: { id: 'ck7idj39n074o0824o1d3k3b1' } },
+      author: { connect: { id: 'ck7hw1chn02zw0824ms8bym8k' } }
+    }
+  }, '{id text }')
+  console.log(JSON.stringify(jason, null, TAB_SPACES))
 }
 
 wrapper()
