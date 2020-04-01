3fc6603d060ffde60d04e73a521f272803cd5b5e
Jason Jones <jason@brucejones.biz>
2020-04-01 17:04:35 -0400

    support for no-floating-promises

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -5,8 +5,8 @@ import { prisma } from '../src/prisma'
 const client = new ApolloBoost({ uri: 'http://localhost:4000' })
 
 test('Create new user "example@example.com"', async () => {
-  const email = 'example@example.com'
-  const createUser = gql`
+    const email = 'example@example.com'
+    const createUser = gql`
       mutation {
       createUser(data: {
         name: "example",
@@ -16,9 +16,9 @@ test('Create new user "example@example.com"', async () => {
       { user { id } }
     }
   `
-  // populate database
-  await client.mutate({ mutation: createUser })
+    // populate database
+    await client.mutate({ mutation: createUser })
 
-  // check to ensure that user actually was saved to database
-  expect(await prisma.exists.User({ email })).toBeTruthy()
+    // check to ensure that user actually was saved to database
+    expect(await prisma.exists.User({ email })).toBeTruthy()
 })
