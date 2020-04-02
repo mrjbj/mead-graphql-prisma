8378745348d170437b5c0fb8d2ea6684fcd49eac
Jason Jones <jason@brucejones.biz>
2020-04-02 00:46:58 -0400

    bedtime

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,8 +1,49 @@
 import 'cross-fetch/polyfill'
 import ApolloBoost, { gql } from 'apollo-boost'
 import { prisma } from '../src/prisma'
+import bcrypt from 'bcryptjs'
 
 const client = new ApolloBoost({ uri: 'http://localhost:4000' })
+const password = 'GooberPatrol'
+
+// setup before each test
+// 1. delete comments, then posts, then users
+// 2. create keyUser for login
+// 3. create keyPosts (one in draft, one published)
+// 4. create keyComments
+beforeEach(async () => {
+    const keyUserEmail = 'dude@example.com'
+
+    await prisma.mutation.deleteManyComments()
+    await prisma.mutation.deleteManyPosts()
+    await prisma.mutation.deleteManyUsers()
+    // keyUser
+    await prisma.mutation.createUser({
+        data: {
+            name: 'The Dude',
+            email: keyUserEmail,
+            password: bcrypt.hashSync(password),
+        },
+    })
+    // keyPost (draft)
+    await prisma.mutation.createPost({
+        data: {
+            title: 'Test Post (* DRAFT *)',
+            body: 'this is a test post, created with published set to false',
+            published: false,
+            author: { connect: { email: keyUserEmail } },
+        },
+    })
+    // keyPost (published)
+    await prisma.mutation.createPost({
+        data: {
+            title: 'Test Post (Published)',
+            body: 'this is a test post, created with published status',
+            published: true,
+            author: { connect: { email: keyUserEmail } },
+        },
+    })
+})
 
 test('Create new user "example@example.com"', async () => {
     const email = 'example@example.com'
